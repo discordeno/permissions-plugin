951abc99eae6c9d84c99dcddf71a71c75d5206af
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-03 16:23:42 +0000

    feat: add channelOverwriteHasPermission

--- src/permissions.ts
+++ src/permissions.ts
@@ -418,3 +418,33 @@ export function isHigherPosition(
     compareRoleId,
   );
 }
+
+/** Checks if a channel overwrite for a user id or a role id has permission in this channel */
+export function channelOverwriteHasPermission(
+  guildId: bigint,
+  id: bigint,
+  overwrites: bigint[],
+  permissions: PermissionStrings[]
+) {
+  const overwrite =
+    overwrites.find((perm) => {
+      const [_, bitID] = separateOverwrites(perm);
+      return id === bitID;
+    }) ||
+    overwrites.find((perm) => {
+      const [_, bitID] = separateOverwrites(perm);
+      return bitID === guildId;
+    });
+
+  if (!overwrite) return false;
+
+  return permissions.every((perm) => {
+    const [_type, _id, allowBits, denyBits] = separateOverwrites(overwrite);
+    if (BigInt(denyBits) & BigInt(BitwisePermissionFlags[perm])) {
+      return false;
+    }
+    if (BigInt(allowBits) & BigInt(BitwisePermissionFlags[perm])) {
+      return true;
+    }
+  });
+}
