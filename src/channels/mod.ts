b9c66c81779a9cd2115cb7d7237c536ffef9a5e5
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-04 17:07:51 +0000

    feat: edit channel and latest versions

--- src/channels/mod.ts
+++ src/channels/mod.ts
@@ -3,10 +3,12 @@ import setupThreadPermChecks from "./threads/mod.ts";
 import setupStagePermChecks from "./stage.ts";
 import deleteChannel from "./deleteChannel.ts";
 import deleteChannelOverwrite from "./deleteChannelOverwrite.ts";
+import editChannel from "./editChannel.ts";
 
 export default function setupChannelPermChecks(bot: BotWithCache) {
     setupThreadPermChecks(bot);
     setupStagePermChecks(bot);
     deleteChannel(bot);
     deleteChannelOverwrite(bot);
+    editChannel(bot);
 }
