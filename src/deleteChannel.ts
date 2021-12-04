2eb6373eba928219624025cafab1b12a3ffb8529
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-04 14:18:11 +0000

    fix: deleteChannel threads perm check

--- src/deleteChannel.ts
+++ src/deleteChannel.ts
@@ -1,14 +1,14 @@
-import { BotWithCache } from "../deps.ts";
+import { BotWithCache, ChannelTypes } from "../deps.ts";
 import { requireBotGuildPermissions } from "./permissions.ts";
 
 export default function deleteChannel(bot: BotWithCache) {
   const deleteChannelOld = bot.helpers.deleteChannel;
 
-  bot.helpers.deleteChannel = async function (channelId, reason) {
-    const channel = await bot.channels.get(channelId);
+  bot.helpers.deleteChannel = function (channelId, reason) {
+    const channel = bot.channels.get(channelId);
 
     if (channel?.guildId) {
-      const guild = await bot.guilds.get(channel.guildId);
+      const guild = bot.guilds.get(channel.guildId);
       if (!guild) throw new Error("GUILD_NOT_FOUND");
 
       if (guild.rulesChannelId === channelId) {
@@ -19,7 +19,17 @@ export default function deleteChannel(bot: BotWithCache) {
         throw new Error("UPDATES_CHANNEL_CANNOT_BE_DELETED");
       }
 
-      await requireBotGuildPermissions(bot, guild, ["MANAGE_CHANNELS"]);
+      const isThread = [
+        ChannelTypes.GuildNewsThread,
+        ChannelTypes.GuildPublicThread,
+        ChannelTypes.GuildPrivateThread,
+      ].includes(channel.type);
+
+      requireBotGuildPermissions(
+        bot,
+        guild,
+        isThread ? ["MANAGE_THREADS"] : ["MANAGE_CHANNELS"],
+      );
     }
 
     return deleteChannelOld(channelId, reason);
