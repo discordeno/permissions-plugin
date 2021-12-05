f70252796839a3edfacf7e096928567f9fc64a34
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-05 15:18:47 +0000

    feat: some guild checks

--- src/guilds/mod.ts
+++ src/guilds/mod.ts
@@ -1,6 +1,12 @@
 import { BotWithCache } from "../../deps.ts";
 import setupEventsPermChecks from "./events.ts";
+import createGuild from "./createGuild.ts";
+import deleteGuild from "./deleteGuild.ts";
+import editGuild from "./editGuild.ts";
 
 export default function setupChannelPermChecks(bot: BotWithCache) {
     setupEventsPermChecks(bot);
+    createGuild(bot);
+    deleteGuild(bot);
+    editGuild(bot);
 }
