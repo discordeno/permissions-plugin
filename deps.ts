890797714bffdca467b29654402f59cb3d38d2ed
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-23 18:11:30 +0000

    chore: rc16

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc15/mod.ts";
-export type { BotWithCache } from "https://deno.land/x/discordeno_cache_plugin@0.0.18/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc16/mod.ts";
+export type { BotWithCache } from "https://deno.land/x/discordeno_cache_plugin@0.0.19/mod.ts";
