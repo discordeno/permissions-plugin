bc56a0d6ca41140a116e6c4c2eb044e66c114803
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-23 14:42:23 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc16/mod.ts";
-export type { BotWithCache } from "https://deno.land/x/discordeno_cache_plugin@0.0.19/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc17/mod.ts";
+export type { BotWithCache } from "https://deno.land/x/discordeno_cache_plugin@0.0.20/mod.ts";
